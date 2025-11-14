453fc4c0e3228fc79ed641509de0a3897b7f315e
digiot-abc <digiot-abc@users.noreply.github.com>
2025-11-14 06:46:02 +0000

    deploy: dfa03f8cef6b2010e5aa218eedde1ef3beb73154

---
+++ _app/immutable/chunks/B7NdpUfh.js
@@ -0,0 +1,1 @@
+import{s as e}from"./5eCYvF9z.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
